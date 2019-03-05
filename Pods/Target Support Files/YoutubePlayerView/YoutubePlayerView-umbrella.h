268ca6fdffea48699eba9bd1c03215e1da6f234e
Yuri Ivashin <yuri.ivashin@icloud.com>
2019-03-05 13:12:15 +0200

    pod update. Remove ‘YoutubePlayer-in-WKWebView’, instead added ‘YoutubePlayerView’

--- Pods/Target Support Files/YoutubePlayerView/YoutubePlayerView-umbrella.h
+++ Pods/Target Support Files/YoutubePlayerView/YoutubePlayerView-umbrella.h
@@ -10,8 +10,7 @@
 #endif
 #endif
 
-#import "WKYTPlayerView.h"
 
-FOUNDATION_EXPORT double YoutubePlayer_in_WKWebViewVersionNumber;
-FOUNDATION_EXPORT const unsigned char YoutubePlayer_in_WKWebViewVersionString[];
+FOUNDATION_EXPORT double YoutubePlayerViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char YoutubePlayerViewVersionString[];
 
